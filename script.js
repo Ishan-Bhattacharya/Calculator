84376b2c94b8c329ff4d5c1a9bddc07734dff1a0
Ishan-Bhattacharya <ibhattacharya315@gmail.com>
2024-09-16 21:32:45 +0530

    From Ubuntu

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let screen = document.querySelector('#screen');
+
+    let ctrl = document.querySelector("#controls");
+    while (true) {
+
+        let screenText = screen.textContent;
+        console.log(screenText);
+        
+        let num1 = 0;
+        let num3 = 0;
+
+    }
+});
